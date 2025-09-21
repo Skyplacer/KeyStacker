5126121cf4a819e2a33e3b712e6c6bb64dbb3a3c
Pahan Amarasekara <pahan.rivertune@gmail.com>
2025-09-22 02:17:04 +0530

    Fixed Constraints on all screens, Did first test on phone, delay and button click navigation made

--- app/src/main/java/com/example/keystacker/Login.kt
+++ app/src/main/java/com/example/keystacker/Login.kt
@@ -1,11 +1,19 @@
 package com.example.keystacker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn = findViewById<Button>(R.id.Login)
+        btn.setOnClickListener {
+            val intent = Intent(this, Homescreen::class.java)
+            startActivity(intent)
+        }
     }
 }
